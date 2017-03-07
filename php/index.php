cbde25c3c53f7cffa3ff00e08477cb0717dbc320
Tom Clark <thrclark@iu.edu>
2017-03-07 11:00:41 -0500

    Index page added

---
+++ php/index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>IRAC</title>
+<link href="../less/styles.css" rel="stylesheet" type="text/css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.iu.edu/style.css?family=BentonSans:regular,bold|BentonSansCond:regular|GeorgiaPro:regular" media="screen" rel="stylesheet" type="text/css"/>
+
+</head>
+<body id="top" class="view-admin">
+<div class="wrapper">
+
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h1 style="margin-top:11px; color:#ffffff; font-size:32px">Infrastructure Resource Allocation Center</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="main-content container" role="main" style="padding-top:20px">
+        <div id="main-content">
+     
+     
+     
+     
+     
+     
+     
+<p class="lead">Prototypes</p>
+<ul>
+    <li>    <a href="irac-editview.php" target="_blank">Edit Mode</a>
+       </li>
+    <li><a href="irac-approveview.php" target="_blank">Approver Mode </a></li>
+</ul>
+        </div>
+    </div>
+
+</div>
+<script type="text/javascript" src="../js/jquery.js"></script> 
+<script type="text/javascript" src="../js/bootstrap.min.js"></script> 
+
+
+</body>
+</html>
